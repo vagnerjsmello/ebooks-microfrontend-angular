4c541850f07149cf9723da3c0381bc964c16fed7
Vagner José Santos de Mello <vagnerjsmello@gmail.com>
2025-04-10 21:47:02 +0100

    feat: estrutura inicial com host-app, MFEs e fake APIs

---
+++ ebooks/mfe-ebook-details/src/app/ebook-details/ebook-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-ebook-details',
+  templateUrl: './ebook-details.component.html',
+  styleUrls: ['./ebook-details.component.scss'],
+})
+export class EbookDetailsComponent {}
+
---
+++ host-app/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    // @ts-ignore
+    loadComponent: () =>
+      import('mfe-ebooks/HomeComponent').then(m => m.HomeComponent),
+  },
+  {
+    path: 'details',
+    // @ts-ignore
+    loadComponent: () =>
+      import('mfe-ebook-details/EbookDetailsComponent').then(m => m.EbookDetailsComponent),
+  }
+];
+
